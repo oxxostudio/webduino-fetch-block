99d9603d2a4da599828fc5cffa4a3948556b3f79
oxxo <oxxo.studio@gmail.com>
2019-02-12 15:59:30 +0800

    Git init

---
+++ blockly/blocks.js
@@ -0,0 +1,26 @@
+Blockly.Blocks['web_fetch'] = {
+  init: function() {
+    this.appendValueInput("data_")
+        .setCheck(null)
+        .appendField("取得內容");
+    this.appendStatementInput("do_")
+        .setCheck(null)
+        .appendField("執行");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(130);
+ this.setTooltip("");
+ this.setHelpUrl("https://oxxo.studio");
+  }
+};
+
+Blockly.Blocks['web_fetch_val'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("取得的內容");
+    this.setOutput(true, null);
+    this.setColour(105);
+ this.setTooltip("");
+ this.setHelpUrl("https://oxxo.studio");
+  }
+};
---
+++ blockly/javascript.js
@@ -0,0 +1,16 @@
+Blockly.JavaScript['web_fetch'] = function(block) {
+  let value_data_ = Blockly.JavaScript.valueToCode(block, 'data_', Blockly.JavaScript.ORDER_ATOMIC);
+  let statements_do_ = Blockly.JavaScript.statementToCode(block, 'do_');
+  let code = 'fetch(\''+value_data_+'\')\n'+
+  '.then(response => {\n'+
+  '  return response.json();\n'+
+  '}).then(myJSON => {\n'+
+  statements_do_ +'\n'+
+  '});';
+  return code;
+};
+
+Blockly.JavaScript['web_fetch_val'] = function(block) {
+  var code = 'myJSON';
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
